a47a861ad1ededb5bab1b5cf845585550b548601
20141105072 <2196690899@qq.com>
2016-06-20 10:25:15 +0800

    summation
    swift不需要引入头文件或者写在main（）里，也不需要在每一句后面加上分号，语法简便，类似于脚本语言。swift使得代码更干净，不易犯错误。
    像打印hello world只用一行代码就可以实现print（hello world）

---
+++ summation/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  summation
+//
+//  Created by 20141105072 on 16/6/20.
+//  Copyright © 2016年 20141105072. All rights reserved.
+//
+
+import Foundation
+var i=1
+var sum=0
+for i in 1...100
+{
+    sum=sum+i
+}
+print(sum)
+
